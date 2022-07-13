99d27399216023dc28dc837c3236bef6c8677aaf
Bill <billyen2012@gmail.com>
2022-07-13 16:44:40 +0800

    update style

--- web-client/src/views/Home.tsx
+++ web-client/src/views/Home.tsx
@@ -94,7 +94,7 @@ export default function Home() {
             data={getUsers()}
             title={
               <div className="flex justify-between items-center">
-                <h3>User</h3>
+                <h3 className='text-gray-600'>USERS</h3>
                 <div className="pt-2 relative text-gray-600">
                   <input value={filter} onChange={(handleFilterChange)} className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
                     placeholder="type to search" />
